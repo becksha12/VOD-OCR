a551d5e6f8be80c4b5b714718d92c45e2e45c164
Saqib Shamsi <shamsi.saqib@gmail.com>
2022-11-23 11:29:00 +0530

    Completed ad break detection

--- detect_ads.py
+++ detect_ads.py
@@ -10,6 +10,7 @@ PLAYBAR_COLOR_BOUNDARY_WHITE = ([253, 253, 253], [255, 255, 255])
 PLAYBAR_COLOR_BOUNDARY_GRAY = ([75, 75, 75], [77, 77, 77])
 DIGIT_DETECTION_CONFIG = '--oem 1 --psm 7 -c tessedit_char_whitelist=0123456789'
 TIME_DETECTION_CONFIG = '--oem 1 --psm 7 -c tessedit_char_whitelist=0123456789/:'
+AD_BREAK_CIRCLE_AREA_RANGE = (20, 35)
 
 
 def read_image(image_path):
@@ -83,22 +84,20 @@ def detect_playbar(image, color_boundary_gray, color_boundary_white):
     return mask
 
 
-def main_timer_detection():
-    IMAGE_PATH_0 = './data/add.PNG'
-    IMAGE_PATH_1 = './data/add-ex-1.PNG'
-    IMAGE_PATH_2 = './data/add-ex-2.PNG'
-    IMAGE_PATH_3 = './data/add-ex-3.PNG'
-    NO_AD_1 = './data/get_add.PNG'
-    NO_AD_2 = './data/beast_peacock-ss_1.png'
-    image = read_image(IMAGE_PATH_3)
-    ret = detect_ad_circle(image, AD_CIRCLE_COLOR_BOUNDARY_ORANGE, AD_CIRCLE_COLOR_BOUNDARY_GRAY)
-    if ret:
-        mask, circle = ret
-        square = get_timer_window_square_from_circle(circle)
-        window = extract_timer_window(image, square)
-        print(detect_ad_time(window, True))
-    else:
-        print('Not an Ad')
+def detect_ad_breaks(image, playbar_y_coord):
+    ad_break_mask = detect_color_mask(image, AD_CIRCLE_COLOR_BOUNDARY_ORANGE)
+    ad_break_mask = cv2.bitwise_not(ad_break_mask)
+    cv2.imwrite('points_mask.png', ad_break_mask)
+    # Find contours
+    # findcontours
+    cnts = cv2.findContours(ad_break_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    ad_coordinates = []
+    for cnt in cnts:
+        area = cv2.contourArea(cnt)
+        ((x, y), r) = cv2.minEnclosingCircle(cnt)
+        if AD_BREAK_CIRCLE_AREA_RANGE[0] <= area <= AD_BREAK_CIRCLE_AREA_RANGE[1] and abs(playbar_y_coord - y) <= 6:
+            ad_coordinates.append((x, y))
+    return ad_coordinates
 
 
 def get_lowest_horizontal_line(lines, length_threshold):
@@ -117,7 +116,6 @@ def get_lowest_horizontal_line(lines, length_threshold):
                 combined_groups[cks].extend(groups[key])
 
     groups = {int(sum(cks)/len(cks)): combined_groups[cks] for cks in combined_groups}
-    print(groups)
 
     continuous_lines = dict()
     for key in groups:
@@ -126,7 +124,6 @@ def get_lowest_horizontal_line(lines, length_threshold):
         if max_x - min_x >= length_threshold:
             continuous_lines[key] = (max_x, min_x)
 
-    print(continuous_lines)
     max_y = max(continuous_lines.keys())
     max_x, min_x = continuous_lines[max_y]
     return (min_x, max_y), (max_x, max_y)
@@ -158,12 +155,51 @@ def coalesce(keys, max_difference=3):
 
 
 def get_playbar_coordinates(playbar_mask):
-    edges = cv2.Canny(playbar_mask, 80, 120)
-    edges = playbar_mask
-    # lines1 = cv2.HoughLinesP(playbar_mask, rho=1, theta=math.pi / 2, threshold=200)
-    lines2 = cv2.HoughLinesP(edges, rho=1, theta=math.pi / 2, threshold=70)
+    lines2 = cv2.HoughLinesP(playbar_mask, rho=1, theta=math.pi / 2, threshold=70)
+    if lines2 is None:
+        return None
     lowest_horizontal_line = get_lowest_horizontal_line(lines2, 900)
     return lowest_horizontal_line
+
+
+def parse_playtime(time_string):
+    current_time, total_time = time_string.split('/')
+    hh, mm, ss = total_time.split(':')
+    return int(hh), int(mm), int(ss)
+
+
+def hhmmss_to_seconds(hh, mm, ss):
+    return hh * 3600 + mm * 60 + ss
+
+
+def seconds_to_hhmmss(seconds):
+    hh = int(seconds / 3600)
+    remaining = seconds - (hh * 3600)
+    mm = int(remaining / 60)
+    remaining = remaining - (mm * 60)
+    return hh, mm, remaining
+
+
+def fraction_of_total(point, start, end):
+    return (point - start) / (end - start)
+
+
+def main_timer_detection():
+    IMAGE_PATH_0 = './data/add.PNG'
+    IMAGE_PATH_1 = './data/add-ex-1.PNG'
+    IMAGE_PATH_2 = './data/add-ex-2.PNG'
+    IMAGE_PATH_3 = './data/add-ex-3.PNG'
+    NO_AD_1 = './data/get_add.PNG'
+    NO_AD_2 = './data/beast_peacock-ss_1.png'
+    image = read_image(IMAGE_PATH_3)
+    ret = detect_ad_circle(image, AD_CIRCLE_COLOR_BOUNDARY_ORANGE, AD_CIRCLE_COLOR_BOUNDARY_GRAY)
+    if ret:
+        mask, circle = ret
+        square = get_timer_window_square_from_circle(circle)
+        window = extract_timer_window(image, square)
+        print(detect_ad_time(window, True))
+    else:
+        print('Not an Ad')
 
 
 def main_playbar_ads_detection():
@@ -171,19 +207,31 @@ def main_playbar_ads_detection():
     IMAGE_2 = './data/add-break-1.PNG'
     image = read_image(IMAGE_1)
     mask = detect_playbar(image, PLAYBAR_COLOR_BOUNDARY_GRAY, PLAYBAR_COLOR_BOUNDARY_WHITE)
-    cv2.imwrite('playbar_mask.png', mask)
-    (x_min, y1), (x_max, y2) = get_playbar_coordinates(mask)
-    print((x_min, y1), (x_max, y2))
-    timer_area = get_playback_time_area(image, x_min, x_max, y1)
-    print(pytesseract.image_to_string(timer_area, lang='eng', config=TIME_DETECTION_CONFIG))
-    cv2.imwrite('timer_area.png', timer_area)
+    coordinates = get_playbar_coordinates(mask)
+    if coordinates is not None:
+        (x_min, y1), (x_max, y2) = coordinates
+        timer_area = get_playback_time_area(image, x_min, x_max, y1)
+        playtime = pytesseract.image_to_string(timer_area, lang='eng', config=TIME_DETECTION_CONFIG)
+        # print(playtime)
+        ad_coords = detect_ad_breaks(image, y1)
+        ad_coords.sort(key=lambda x: x[0])
+        # print(ad_coords)
+        hh, mm, ss = parse_playtime(playtime)
+        playtime_in_seconds = hhmmss_to_seconds(hh, mm, ss)
+        for i, (x, y) in enumerate(ad_coords):
+            add_at_point = max(0, fraction_of_total(x, x_min, x_max)) # TODO: adjust the minimum
+            add_at_time = int(add_at_point * playtime_in_seconds)
+            h, m, s = seconds_to_hhmmss(add_at_time)
+            print(f'Add #{i+1} at: {add_at_time}s ({h:02d}:{m:02d}:{s:02d})')
+    else:
+        print('Playbar not detected')
+
     # ((33, 704), (1835, 704))
 
 
 if __name__ == '__main__':
     main_playbar_ads_detection()
     # keys = [701, 700, 704, 702, 703, 699, 217, 215, 207, 216, 303]
-    # print(coalesce(keys))
     # Test pick circles function
     # print(pick_bottom_left_circle(np.array([[[25, 35, 12.5]]])) == [25, 35, 12.5])
     # print(pick_bottom_left_circle(np.array([[[25, 35, 12.5], [25, 100, 12.5], [35, 120, 12.5]]])) == [25, 100, 12.5])
