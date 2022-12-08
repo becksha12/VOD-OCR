775cb5cdf165d1261258fccc750018c6e26ca118
Saqib Shamsi <shamsi.saqib@gmail.com>
2022-12-08 14:51:41 +0530

    Added functionality to specify the number of frames to extract

--- video_utils.py
+++ video_utils.py
@@ -2,13 +2,17 @@ import cv2
 import math
 
 
-def get_frame_for_every_second(video_path):
+def get_frame_for_every_second(video_path, max_frames=None):
     vid_ob = cv2.VideoCapture(video_path)
     frame_rate = vid_ob.get(5)
-    while vid_ob.isOpened():
+    if max_frames is None:
+        max_frames = math.inf
+    current_frame = 0
+    while vid_ob.isOpened() and current_frame < max_frames:
         frame_id = vid_ob.get(1)
         ret, frame = vid_ob.read()
         if ret:
           if frame_id % math.floor(frame_rate) == 0:
+              current_frame += 1
               yield frame
     vid_ob.release()
